1f3abca3dec770af34efbc01454a6e4b5dbddddc
o2165471 <pierre.mvondo@etu.univ-orleans.fr>
2022-01-15 19:11:26 +0100

    rajout de la musique version 1

--- clientjfx/src/main/java/application/App.java
+++ clientjfx/src/main/java/application/App.java
@@ -2,13 +2,19 @@ package application;
 
 import controleur.Controleur;
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    MediaPlayer mediaPlayer;
     @Override
     public void start(Stage stage) {
         Controleur controleur = new Controleur(stage);
         controleur.run();
+        Media media= new Media("file:///D://CoursM1_2021_2022/projets1/clientjfx/src/main/resources/vues/music/akatsuki.mp3");
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
     }
 
     public static void main(String[] args) {

70b24bd58eeff927ace8b9b305d538181eb3f784
Ndoye Momar <momar.ndoye@etu.univ-orleans.fr>
2022-01-17 19:10:54 +0100

    ajout music

--- clientjfx/src/main/java/application/App.java
+++ clientjfx/src/main/java/application/App.java
@@ -2,15 +2,20 @@ package application;
 
 import controleur.Controleur;
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    MediaPlayer mediaPlayer;
     @Override
     public void start(Stage stage) {
         Controleur controleur = new Controleur(stage);
         controleur.run();
+        Media media = new Media("music/akatsuki.mp3");
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
     }
-
     public static void main(String[] args) {
         launch();
 

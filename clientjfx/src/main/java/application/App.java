9444c676e1a090eb8921dc4ec8c7a9fe0597b8b7
Ndoye Momar <momar.ndoye@etu.univ-orleans.fr>
2022-01-17 19:27:23 +0100

    modification App.java

--- clientjfx/src/main/java/application/App.java
+++ clientjfx/src/main/java/application/App.java
@@ -6,15 +6,18 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 public class App extends Application {
-    MediaPlayer mediaPlayer;
+    //MediaPlayer mediaPlayer;
     @Override
     public void start(Stage stage) {
         Controleur controleur = new Controleur(stage);
         controleur.run();
-        Media media= new Media("file:///D://CoursM1_2021_2022/projets1/clientjfx/src/main/resources/vues/music/akatsuki.mp3");
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
+        //Media media= new Media(new File("clientjfx/src/main/resources/vues/music/akatsuki.mp3").getAbsolutePath());
+        //mediaPlayer = new MediaPlayer(media);
+        //mediaPlayer.setAutoPlay(true);
     }
 
     public static void main(String[] args) {
